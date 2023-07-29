d2351a579f95550fa6adec9e300b5d73292c5ec1
saravanakumar <c.v.saravanakumar247@gmail.com>
2023-07-29 16:43:14 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'Which of these is the shortest time span?',
+			answerOptions: [
+				{ answerText: 'Century', isCorrect: false },
+				{ answerText: 'A Dozen year', isCorrect: false },
+				{ answerText: 'Decade', isCorrect: true },
+				{ answerText: 'Millennium', isCorrect: false },
+			],
+		},
+		{
+			questionText: ' Which is the fastest bird?',
+			answerOptions: [
+				{ answerText: 'Hawk', isCorrect: false },
+				{ answerText: 'Eagle', isCorrect: true },
+				{ answerText: 'Perigrine Falcon', isCorrect: false },
+				{ answerText: 'Stork', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which is the Capital of India?',
+			answerOptions: [
+				{ answerText: 'New delhi', isCorrect: true },
+				{ answerText: 'Mumbai', isCorrect: false },
+				{ answerText: 'Banglore', isCorrect: false },
+				{ answerText: 'Indore', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which is the fastest fish',
+			answerOptions: [
+				{ answerText: 'Shark', isCorrect: false },
+				{ answerText: 'Tuna', isCorrect: false },
+				{ answerText: 'Flying fish', isCorrect: false },
+				{ answerText: 'Sailfish', isCorrect: true },
+			],
+		},
+	];
+
+  const[currentQuestion,setcurrentQuestion]= useState(0);
+
+  const[showScore,setShowScore]= useState(false);
+
+  const[score,setScore]= useState(0);
+
+  const handleChange = (isCorrect) =>{
+    if(isCorrect===true)
+    setScore(score+1);
+
+    const nextQuestion =currentQuestion+1;
+    if(nextQuestion < questions.length){
+      setcurrentQuestion(nextQuestion);
+    }
+    else{
+      setShowScore(true)
+    }
+    
+  }
+
+	return (
+   
+		<div className='app'>
+			
+			{showScore ? (
+				<div className='score-section'>You scored {score} out of {questions.length}</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion+1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption)=>(
+            <button onClick={() => handleChange(answerOption.isCorrect)}>{answerOption.answerText}</button>
+            ))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
